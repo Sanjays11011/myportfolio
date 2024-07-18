58d947f72d71c96df98c707ca931224f6111db18
Sanjay S <sanjays13408@gmail.com>
2024-07-18 19:57:27 +0530

    changed

--- src/component/Skills.js
+++ src/component/Skills.js
@@ -49,13 +49,13 @@ export default function Skills() {
             alt: "c"
         }
     ]
-    return <section className="text-[#5f5e5e] w-svw h-svh bg-white">
+    return <section className="text-[#5f5e5e] w-svw h-svh bg-white  ">
         <div className="w-full h-1/6">
-        <h1 className="font-bold text-5xl text-lime-400 px-20 py-10 flex justify-center">Skills Known</h1>
+        <h1 className="font-bold text-5xl text-lime-400 px-20 py-10 text-center">Skills Known</h1>
         </div>
-        <div className="w-full md:h-1/4 h-1/3">
+        <div className="w-full md:w-1/2 md:h-1/4 h-1/3 ">
         <h1 className="font-bold text-3xl md:px-20 md:py-0 md:pb-4 pt-16 pb-4 px-5 flex">Front-End</h1>
-        <div className="w-3/4 md:w-1/2 md:h-1/3 md:py-3  flex flex-wrap ml-3 md:gap-4 gap-4 md:items-center">
+        <div className="w-full md:w-full md:h-1/3 md:py-3  flex flex-wrap ml-3 md:gap-4 gap-4 md:items-center">
         {images.map((image, index) => (
             <img key={index} className="w-16 h-16 md:w-0.7/4 md:h-16" src={image.src} alt={image.alt}/>
         ))}
@@ -73,7 +73,7 @@ export default function Skills() {
         }
             </div>
         </div>
-        <div className="md:mt-5 w-full md:h-1/4 h-1/3">
+        <div className=" w-1/2 md:h-1/4 h-1/3">
         <h1 className="font-bold text-3xl md:px-20 md:py-0 px-5 flex">Database</h1>
         <div className="w-1/2 h-1/3 md:h-3/4  flex flex-row ml-3 md:gap-2 gap-4 md:items-center">
             <img className='w-16 h-16 md:w-0.7/4 md:h-3/4' src={mysql} alt="mysql"/>
